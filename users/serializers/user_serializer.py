d73273ad931ea70abb0de8e4e382391ebc67cc67
elsouleymane <el.nuhu@uvci.edu.ci>
2024-10-16 07:30:25 +0000

    feature/sn/#1-api-user-authentication

--- users/serializers/user_serializer.py
+++ users/serializers/user_serializer.py
@@ -6,3 +6,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserModels
         fields = "__all__"
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = UserModels
+
+    """
+    Serializer for password change endpoint.
+    """
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
